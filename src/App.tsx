a4cf5acb3519d1ae776e5f6f7100bd7dfa12e186
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-10-12 22:48:24 -0700

    Initial Commit
    Just went haywire and tried building something like STE's editor stack, but fully with SolidJS signal primitives. Working great! I first tried doing this a bit in React too, but then I wanted to try `createStore()` in Solid. That got complex, so I just went with `createSignal()` and did the props management the React way, by assigning a new array with each mutation. Not ideal, but works nice enough for now I guess. The editor refs themselves are the same objects, so that's good too.

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import { createSignal } from "solid-js";
+import indexURL from "./index.js?url";
+import htmlURL from "../index.html?url";
+import "./App.css";
+
+import type { Accessor, Setter } from "solid-js";
+
+const [index, html] = await Promise.all([indexURL, htmlURL].map(url => fetch(url).then(response => response.text())));
+
+export default function App() {
+  const [editors, setEditors] = createSignal<EditorState[]>([]);
+
+  setEditors(previous => [...previous, new EditorState({ name: "index.js", value: index }), new EditorState({ name: "index.html", value: html })]);
+
+  return (
+    <div class="App">
+      <Workspace editors={editors} setEditors={setEditors}/>
+    </div>
+  );
+}
+
+function Workspace(props: { editors: Accessor<EditorState[]>; setEditors: Setter<EditorState[]>; }) {
+  return (
+    <div class="Workspace">
+      <Tabs editors={props.editors} setEditors={props.setEditors}/>
+      <Editors editors={props.editors}/>
+    </div>
+  );
+}
+
+function Tabs(props: { editors: Accessor<EditorState[]>; setEditors: Setter<EditorState[]>; }) {
+  return (
+    <div class="Tabs">
+      {props.editors().map(editor => <Tab editor={editor} setEditors={props.setEditors}/>)}
+      <Menus setEditors={props.setEditors}/>
+    </div>
+  );
+}
+
+function Tab(props: { editor: EditorState; setEditors: Setter<EditorState[]>; }) {
+  return (
+    <div class="Tab">
+      <button>{props.editor.name}</button>
+      <button onclick={() => props.setEditors(previous => previous.filter(editor => editor.uuid !== props.editor.uuid))}>×</button>
+    </div>
+  );
+}
+
+function Menus(props: { setEditors: Setter<EditorState[]>; }) {
+  return (
+    <div class="Menus">
+      <Menu onclick={async () => {
+        const files: File[] = await openFile({ multiple: true });
+        const entries: [string, string][] = await Promise.all(files.map(async file => [file.name, await file.text()]));
+        const editors: EditorState[] = entries.map(([name, value]) => new EditorState({ name, value }));
+        props.setEditors(previous => [...previous, ...editors]);
+      }}>📎</Menu>
+      <Menu onclick={() => props.setEditors(previous => [...previous, new EditorState({ name: "Untitled.txt", value: `Hello world!\n${crypto.randomUUID()}` })])}>+</Menu>
+    </div>
+  );
+}
+
+function Menu(props: { onclick: () => void; children: string; }) {
+  return (
+    <button class="Menu" onclick={props.onclick}>{props.children}</button>
+  );
+}
+
+function Editors(props: { editors: Accessor<EditorState[]>; }) {
+  return (
+    <div class="Editors">
+      {props.editors().map(editor => <Editor editor={editor}/>)}
+    </div>
+  );
+}
+
+function Editor(props: { editor: EditorState; }) {
+  return (
+    <div class="Editor">
+      <ol></ol>
+      <textarea>{props.editor.value}</textarea>
+    </div>
+  );
+}
+
+class EditorState {
+  name: string;
+  value: string;
+  readonly uuid = crypto.randomUUID();
+
+  constructor({ name, value }: { name: string, value: string }) {
+    this.name = name;
+    this.value = value;
+  }
+}
+
+async function openFile({ multiple = false }: { multiple?: boolean; }): Promise<File[]> {
+  return await new Promise<File[]>((resolve, reject) => {
+    const input: HTMLInputElement = document.createElement("input");
+    input.type = "file";
+    input.multiple = multiple;
+    input.addEventListener("change", () => resolve([...input.files!]), { once: true });
+    input.addEventListener("error", reject, { once: true });
+    input.click();
+  });
+}
